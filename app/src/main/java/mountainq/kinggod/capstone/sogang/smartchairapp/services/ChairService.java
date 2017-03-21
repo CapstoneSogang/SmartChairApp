5282a5a3bf8da9f9492a87b0ba0dfe7d6d59a168
geusan <dnay2k@gmail.com>
2017-03-21 18:21:51 +0900

    add ChairService

--- app/src/main/java/mountainq/kinggod/capstone/sogang/smartchairapp/services/ChairService.java
+++ app/src/main/java/mountainq/kinggod/capstone/sogang/smartchairapp/services/ChairService.java
@@ -1,11 +1,25 @@
 package mountainq.kinggod.capstone.sogang.smartchairapp.services;
 
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.philips.lighting.hue.sdk.PHAccessPoint;
+import com.philips.lighting.hue.sdk.PHBridgeSearchManager;
 
+import java.util.ArrayList;
 import java.util.Map;
 
+import mountainq.kinggod.capstone.sogang.smartchairapp.MainActivity;
 import mountainq.kinggod.capstone.sogang.smartchairapp.managers.HueManager;
+import mountainq.kinggod.capstone.sogang.smartchairapp.managers.PropertyManager;
 
 /**
  * Created by dnay2 on 2017-03-19.
@@ -13,7 +27,21 @@ import mountainq.kinggod.capstone.sogang.smartchairapp.managers.HueManager;
 
 public class ChairService extends FirebaseMessagingService {
 
+    PropertyManager propertyManager = PropertyManager.getInstance();
     HueManager hueManager = HueManager.getInstance();
+    PHBridgeSearchManager sm;
+    PHAccessPoint accessPoint;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ArrayList<PHAccessPoint> list = hueManager.searchPhHueDevice();
+        if(list.size() > 0) {
+            showNotification("there are some Hue devices near by here.");
+            for(PHAccessPoint ap : list)
+                propertyManager.setIpAddress(ap.getIpAddress());
+        }
+    }
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
@@ -24,72 +52,53 @@ public class ChairService extends FirebaseMessagingService {
 
     }
 
+    private void showNotification(String message){
+        NotificationCompat.Builder builder = buildSimpleNotification("", "", "Hello Hue", message);
+        builder.build();
+    }
+
 //    의자 움직이는거 만들어야함
 
 
-    //    private NotificationCompat.Builder buildSimpleNotification(String code, String idx, String title, String message) {
-//        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
-//        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+        private NotificationCompat.Builder buildSimpleNotification(String code, String idx, String title, String message) {
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
 //                .setSmallIcon(R.drawable.ic_notice_alarm)
-//                .setContentTitle(title)
-//                .setContentText(message)
-//                .setAutoCancel(true)
-//                .setShowWhen(true)
-//                .setWhen(System.currentTimeMillis())
-//                .setSound(defaultSoundUri)
-//                .setDefaults(Notification.DEFAULT_ALL)
 //                .setColor(StaticDatas.MAIN_COLOR)
-//                .setContentIntent(getPendingIntent(code, idx));
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-//            builder.setCategory(Notification.CATEGORY_MESSAGE)
-//                    .setPriority(Notification.PRIORITY_HIGH)
-//                    .setVisibility(Notification.VISIBILITY_PUBLIC)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setShowWhen(true)
+                .setWhen(System.currentTimeMillis())
+                .setSound(defaultSoundUri)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setContentIntent(getPendingIntent(code, idx));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            builder.setCategory(Notification.CATEGORY_MESSAGE)
 //                    .setColor(StaticDatas.MAIN_COLOR)
-//                    .setSmallIcon(R.drawable.ic_notice_alarm);
-//
-//        }
-//        return builder;
-//    }
-//
-//    private NotificationCompat.Builder buildCustomNotification(String code, String id, String title, String message) {
-//        RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.notificationbar);
-//        remoteViews.setImageViewResource(R.id.logoimg, R.mipmap.ic_launcher);
-//        remoteViews.setTextViewText(R.id.title, title);
-//        remoteViews.setTextViewText(R.id.message, message);
-//
-//        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-//                .setContent(remoteViews)
-//                .setSmallIcon(R.drawable.ic_notice_alarm)
-//                .setAutoCancel(true)
-//                .setWhen(System.currentTimeMillis())
-//                .setShowWhen(true)
-//                .setContentIntent(getPendingIntent(code, id))
-//                .setDefaults(Notification.DEFAULT_ALL);
-//
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-//            notificationBuilder.setCategory(Notification.CATEGORY_MESSAGE)
-//                    .setPriority(Notification.PRIORITY_HIGH)
-//                    .setVisibility(Notification.VISIBILITY_PUBLIC);
-//        }
-//
-//
-//        return notificationBuilder;
-//    }
-//
-//    private PendingIntent getPendingIntent(String code, String idx) {
-//        Intent intent = null;
-//
-//        intent = new Intent(this, MainActivity.class);
-//        intent.putExtra("code", code);
-//        intent.putExtra("idx", idx);
-//
-//        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-//        stackBuilder.addParentStack(LoginActivity.class);
-//        stackBuilder.addNextIntent(intent);
-//
-//        return stackBuilder.getPendingIntent(
-//                0,
-//                PendingIntent.FLAG_UPDATE_CURRENT
-//        );
-//    }
+//                    .setSmallIcon(R.drawable.ic_notice_alarm)
+                    .setPriority(Notification.PRIORITY_HIGH)
+                    .setVisibility(Notification.VISIBILITY_PUBLIC);
+
+
+        }
+        return builder;
+    }
+
+    private PendingIntent getPendingIntent(String code, String idx) {
+        Intent intent = null;
+
+        intent = new Intent(this, MainActivity.class);
+        intent.putExtra("code", code);
+        intent.putExtra("idx", idx);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(MainActivity.class);
+        stackBuilder.addNextIntent(intent);
+
+        return stackBuilder.getPendingIntent(
+                0,
+                PendingIntent.FLAG_UPDATE_CURRENT
+        );
+    }
 }
